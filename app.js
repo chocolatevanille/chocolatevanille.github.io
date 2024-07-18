a90483473d77aaa17de651dd09045ec151687c1e
willster191 <William.C.B.19@gmail.com>
2024-07-18 02:06:55 -0500

    Added sentiment analysis demo

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ const highlightMenu = () => {
         aboutMenu.classList.remove('highlight');
         projectsMenu.classList.remove('highlight');
         return
-    } else if (window.innerWidth > 960 && scrollPos < 7600) {
+    } else if (window.innerWidth > 960 && scrollPos < 8000) {
 
         projectsMenu.classList.add('highlight');
         offerMenu.classList.remove('highlight');
